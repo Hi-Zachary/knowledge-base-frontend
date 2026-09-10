3b6023f9438f37b7ac65655fd061b3f8ffee8750
Hi-Zachary <zacharyzhen@outlook.com>
2026-09-10 18:41:44 +0800

    配置 GitHub Pages 部署

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/knowledge-base-frontend/">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/knowledge-base-frontend/',
   plugins: [react()],
 })
